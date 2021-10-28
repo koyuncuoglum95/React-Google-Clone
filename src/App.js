d86fb688339e2c528b9a724308918c4d869b476c
koyuncuoglum95 <77508734+koyuncuoglum95@users.noreply.github.com>
2021-10-28 15:32:40 -0700

    Google-Clone React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import SearchResult from './pages/SearchResult/SearchResult';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <Router>
+        <Switch>
+
+          <Route path="/" exact>
+            <Home />
+          </Route>
+
+          <Route path="/search">
+            <SearchResult />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
